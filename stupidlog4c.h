a3351bdb2cde2ffe92ed9e43e43f355a84128802
Spencer Peterson <spencerjpeterson@gmail.com>
2018-12-15 16:10:16 -0800

    print timestamp on log lines

--- stupidlog4c.h
+++ stupidlog4c.h
@@ -8,5 +8,9 @@ int stupid_log_close();
 FILE *stupid_log_handle();
 
 #define stupid_log(_level, _fmt, ...) do{\
-	fprintf(stupid_log_handle(), "[%-5s] "_fmt"\n", #_level, ##__VA_ARGS__);\
+	time_t _t = time(NULL);\
+	struct tm _tm; localtime_r(&_t, &_tm);\
+	char _tbuf[32];\
+	_tbuf[strftime(_tbuf, sizeof(_tbuf), "%F %T", &_tm)] = '\0';\
+	fprintf(stupid_log_handle(), "%s %-5s "_fmt"\n", _tbuf, #_level, ##__VA_ARGS__);\
 }while(0)
